951aff0a1d217d84f10790402c2b1e25dce82e69
Yunus ARAS <yunus.kariha@gmail.com>
2015-04-10 16:23:40 +0300

    deneme

---
+++ post-on-timeline.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+require_once( 'Facebook/HttpClients/FacebookHttpable.php' );
+require_once( 'Facebook/HttpClients/FacebookCurl.php' );
+require_once( 'Facebook/HttpClients/FacebookCurlHttpClient.php' );
+require_once( 'Facebook/Entities/AccessToken.php' );
+require_once( 'Facebook/Entities/SignedRequest.php');
+require_once( 'Facebook/FacebookSession.php' );
+require_once( 'Facebook/FacebookSignedRequestFromInputHelper.php');
+require_once( 'Facebook/FacebookCanvasLoginHelper.php');
+require_once( 'Facebook/FacebookRedirectLoginHelper.php' );
+require_once( 'Facebook/FacebookRequest.php' );
+require_once( 'Facebook/FacebookResponse.php' );
+require_once( 'Facebook/FacebookSDKException.php' );
+require_once( 'Facebook/FacebookRequestException.php' );
+require_once( 'Facebook/FacebookOtherException.php' );
+require_once( 'Facebook/FacebookAuthorizationException.php' );
+require_once( 'Facebook/GraphObject.php' );
+require_once( 'Facebook/GraphUser.php');
+require_once( 'Facebook/GraphSessionInfo.php' );
+
+use Facebook\HttpClients\FacebookHttpable;
+use Facebook\HttpClients\FacebookCurl;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+use Facebook\Entities\AccessToken;
+use Facebook\Entities\SignedRequest;
+use Facebook\FacebookSession;
+use Facebook\FacebookSignedRequestFromInputHelper;
+use Facebook\FacebookCanvasLoginHelper;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookOtherException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\GraphUser;
+use Facebook\GraphSessionInfo;
+
+FacebookSession::setDefaultApplication('624449367686374','45e02d54f428fb89a0e5e4821e80c408');
+
+$helper = new FacebookCanvasLoginHelper();
+
+try {
+	$session = $helper->getSession();
+} catch (FacebookRequestException $ex) {
+	echo $ex->getMessage();
+} catch (\Exception $ex) {
+	echo $ex->getMessage();
+}
+
+if ($session) {
+	try {
+		// posting on user timeline
+		$postRequest = new FacebookRequest($session, 'POST', '/me/feed', array('message' => 'This message must come from user end!!!'));
+		$postResponse = $postRequest->execute();
+		$posting = $postResponse->getGraphObject();
+		echo $posting->getProperty('id');
+	} catch(FacebookRequestException $e) {
+		echo $e->getMessage();
+	}
+} else {
+	$helper = new FacebookRedirectLoginHelper('https://apps.facebook.com/ogrencievleri/');
+	$auth_url = $helper->getLoginUrl(array('publish_actions', 'email'));
+	echo "<script>window.top.location.href='".$auth_url."'</script>";
+}
