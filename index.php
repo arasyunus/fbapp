8083c4c83e143b066bd3c578682d5a0bc1c816e0
Yunus ARAS <yunus.kariha@gmail.com>
2015-04-10 16:16:33 +0300

    sdasd

--- index.php
+++ index.php
@@ -1,103 +1,88 @@
 <?php
-session_start();
+/* INCLUSION OF LIBRARY FILEs*/
+	require_once( 'lib/Facebook/FacebookSession.php');
+	require_once( 'lib/Facebook/FacebookRequest.php' );
+	require_once( 'lib/Facebook/FacebookResponse.php' );
+	require_once( 'lib/Facebook/FacebookSDKException.php' );
+	require_once( 'lib/Facebook/FacebookRequestException.php' );
+	require_once( 'lib/Facebook/FacebookRedirectLoginHelper.php');
+	require_once( 'lib/Facebook/FacebookAuthorizationException.php' );
+	require_once( 'lib/Facebook/FacebookRequestException.php' );
+	require_once( 'lib/Facebook/GraphObject.php' );
+	require_once( 'lib/Facebook/GraphUser.php' );
+	require_once( 'lib/Facebook/GraphSessionInfo.php' );
+	require_once( 'lib/Facebook/Entities/AccessToken.php');
+	require_once( 'lib/Facebook/HttpClients/FacebookCurl.php' );
+	require_once( 'lib/Facebook/HttpClients/FacebookHttpable.php');
+	require_once( 'lib/Facebook/HttpClients/FacebookCurlHttpClient.php');
 
-require_once 'facebook-php-sdk/autoload.php';
-use Facebook\FacebookSession;
-use Facebook\FacebookRequest;
-use Facebook\GraphUser;
-use Facebook\FacebookRequestException;
-use Facebook\FacebookRedirectLoginHelper;
+/* USE NAMESPACES */
+	
+	use Facebook\FacebookSession;
+	use Facebook\FacebookRedirectLoginHelper;
+	use Facebook\FacebookRequest;
+        
+	use Facebook\FacebookResponse;
+	use Facebook\FacebookSDKException;
+	use Facebook\FacebookRequestException;
+	use Facebook\FacebookAuthorizationException;
+	use Facebook\GraphObject;
+	use Facebook\GraphUser;
+	use Facebook\GraphSessionInfo;
+	use Facebook\FacebookHttpable;
+	use Facebook\FacebookCurlHttpClient;
+	use Facebook\FacebookCurl;
 
-$api_key = '624449367686374';
-$api_secret = '45e02d54f428fb89a0e5e4821e80c408';
-$redirect_login_url = 'https://ogrencievlerifb.herokuapp.com/';
+/*PROCESS*/
+	
+	//1.Stat Session
+	 session_start();
+	//2.Use app id,secret and redirect url
+	 $app_id = '906233676099560';
+	 $app_secret = '5281f8d25677ced12934a712a4f833e5';
+	 $redirect_url='http://localhost/fb/';
+	 
+	 //3.Initialize application, create helper object and get fb sess
+	 FacebookSession::setDefaultApplication($app_id,$app_secret);
+	 $helper = new FacebookRedirectLoginHelper($redirect_url);
+	 $sess = $helper->getSessionFromRedirect();
 
+        if(isset($sess)){
+            
+            $request = new FacebookRequest($sess, 'GET', '/me');
+            $response = $request->execute();
+            $graph = $response->getGraphObject(GraphUser::className());
+            $name= $graph->getName();
+            echo "hi $name";
 
+if($sess) {
 
-// https://www.webniraj.com/2014/05/01/facebook-api-php-sdk-updated-to-v4-0-0/
+  try {
 
-// initialize your app using your key and secret
-FacebookSession::setDefaultApplication($api_key, $api_secret);
+    $response = (new FacebookRequest(
+    $sess, 'POST', '/me/feed', array(
+      'name' => 'Facebook API: Posting a Status Update Using PHP SDK 4.0.x',
+      'caption' => "I'm rewriting old tutorials to work with the new PHP SDK 4.0 and Graph API 2.0.",
+      'link' => 'https://www.webniraj.com/2014/05/29/facebook-api-p…-php-sdk-4-0-x/',
+      'message' => 'Check out my new tutorial'
+    )
+  ))->execute()->getGraphObject()->asArray();
+ 
+// output response - should include post_id
+print_r( $response );
 
-// create a helper opject which is needed to create a login URL
-// the $redirect_login_url is the page a visitor will come to after login
-$helper = new FacebookRedirectLoginHelper( $redirect_login_url);
+  } catch(FacebookRequestException $e) {
 
+    echo "Exception occured, code: " . $e->getCode();
+    echo " with message: " . $e->getMessage();
 
-// First check if this is an existing PHP session
-if ( isset( $_SESSION ) && isset( $_SESSION['fb_token'] ) ) {
-	// create new session from the existing PHP sesson
-	$session = new FacebookSession( $_SESSION['fb_token'] );
-	try {
-		// validate the access_token to make sure it's still valid
-		if ( !$session->validate() ) $session = null;
-	} catch ( Exception $e ) {
-		// catch any exceptions and set the sesson null
-		$session = null;
-		echo 'No session: '.$e->getMessage();
-	}
-}  elseif ( empty( $session ) ) {
-	// the session is empty, we create a new one
-	try {
-		// the visitor is redirected from the login, let's pickup the session
-		$session = $helper->getSessionFromRedirect();
-	} catch( FacebookRequestException $e ) {
-		// Facebook has returned an error
-		echo 'Facebook (session) request error: '.$e->getMessage();
-	} catch( Exception $e ) {
-		// Any other error
-		echo 'Other (session) request error: '.$e->getMessage();
-	}
+  }   
+
 }
-if ( isset( $session ) ) {
-	// store the session token into a PHP session
-	$_SESSION['fb_token'] = $session->getToken();
-	// and create a new Facebook session using the cururent token
-	// or from the new token we got after login
-	$session = new FacebookSession( $session->getToken() );
-	try {
-		// with this session I will post a message to my own timeline
-		$request = new FacebookRequest(
-			$session,
-			'POST',
-			'/me/feed',
-			array(
-				'link' => 'www.finalwebsites.com/facebook-api-php-tutorial/',
-				'message' => 'A step by step tutorial on how to use Facebook PHP SDK v4.0'
-			)
-		);
-		$response = $request->execute();
-		$graphObject = $response->getGraphObject();
-		// the POST response object
-		echo '<pre>' . print_r( $graphObject, 1 ) . '</pre>';
-		$msgid = $graphObject->getProperty('id');
-	} catch ( FacebookRequestException $e ) {
-		// show any error for this facebook request
-		echo 'Facebook (post) request error: '.$e->getMessage();
+            
+            
+	}else{
+		//else echo login
+		echo '<a href='.$helper->getLoginUrl().'>Login with facebook</a>';
 	}
-	// now we create a second request to get the posted message in return
-	if ( isset ( $msgid ) ) {
-		// we only need to the sec. part of this ID
-		$parts = explode('_', $msgid);
-		try {
-			$request2 = new FacebookRequest(
-				$session,
-				'GET',
-				'/'.$parts[1]
-			);
-			$response2 = $request2->execute();
-			$graphObject2 = $response2->getGraphObject();
-			// the GET response object
-			echo '<pre>' . print_r( $graphObject2, 1 ) . '</pre>';
-		} catch ( FacebookRequestException $e ) {
-			// show any error for this facebook request
-			echo 'Facebook (get) request error: '.$e->getMessage();
-		}
-	}
-} else {
-	// we need to create a new session, provide a login link
-	echo 'No session, please <a href="'. $helper->getLoginUrl( array( 'publish_actions' ) ).'">login</a>.';
-}
 
-// use this for testing only
-//unset($_SESSION['fb_token']);
